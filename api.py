b9f4c122b983d57a29670177b070ce76307264f1
Lennart-Ma <72752538+Lennart-Ma@users.noreply.github.com>
2021-04-09 22:06:37 +0200

    Add starting page routes

--- api.py
+++ api.py
@@ -2,12 +2,23 @@ import os
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 UPLOAD_FOLDER = r"C:\Users\Admin\Documents\GitHub\SkinDiseaseApp\static"
 
 @app.route("/", methods=["GET", "POST"])
-def upload_predict():
+def starting_page(): 
+    return render_template("home.html")
+
+@app.route("/home", methods=["GET", "POST"])
+def home_page():
+    return render_template("home.html")
+
+
+
+@app.route("/multidisease", methods=["GET", "POST"])
+def multidisease():
     if request.method == "POST":
         image_file = request.files["image"] # "image" -> name of the input file in index.html
         # make sure that image_file excists
@@ -17,9 +28,9 @@ def upload_predict():
                 image_file.filename
             )
             image_file.save(image_location)
-            return render_template("index.html", prediction = "You uploaded an image to get predicted")
+            return render_template("multidisease.html", prediction = "You uploaded an image to get predicted")
             
-    return render_template("index.html", prediction = "No Image uploaded")
+    return render_template("multidisease.html", prediction = "No Image uploaded")
 
 
 if __name__ == "__main__":
